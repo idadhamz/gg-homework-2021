bc34a799452c9d2ebf431ed006bf9069e138d7b6
idadhamz <dadiilhamsetiadii@gmail.com>
2021-07-14 22:27:03 +0700

    add color to description components

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -9,13 +9,14 @@ import data from '../../data/index';
 
 const index = () => {
     return (
-        <>
+        <div class="playlist">
             <h1>Data Playlist</h1>
 
-            <div className="playlist">
+            <div className="list-playlist">
+                <PlaylistItem data={data} />
                 <PlaylistItem data={data} />
             </div>
-        </>
+        </div>
     )
 }
 
